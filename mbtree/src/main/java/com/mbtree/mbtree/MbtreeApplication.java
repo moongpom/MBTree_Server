a2f38ae1b8d2e8cb8133ad91121ae2f3041996ff
moongpom <eunmoong@gmail.com>
2022-07-02 11:29:25 +0900

    first gradle setting

---
+++ mbtree/src/main/java/com/mbtree/mbtree/MbtreeApplication.java
@@ -0,0 +1,13 @@
+package com.mbtree.mbtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtreeApplication.class, args);
+	}
+
+}
---
+++ mbtree/src/test/java/com/mbtree/mbtree/MbtreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbtree.mbtree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
